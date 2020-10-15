2f6010f88339a1f29cf386f7f9f59143263a1b91
Tryop-X <65966143+Tryop-X@users.noreply.github.com>
2020-10-15 15:42:28 -0500

    infe

--- src/pruebaGit/PruebaGit.java
+++ src/pruebaGit/PruebaGit.java
@@ -3,6 +3,6 @@ package pruebaGit;
 public class PruebaGit {
 	public static void main( String[] args) {
 		System.out.println("Tienes que rogar a dios");
-		System.out.println("tÃº tambiÃ©n");
+		System.out.println("tú también");
 	}
 }
