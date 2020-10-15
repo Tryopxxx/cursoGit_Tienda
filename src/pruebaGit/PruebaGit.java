a324e2819dddc64a9b12bebadfd5b2f4644cfa6f
Tryopxxx <51171372+Tryopxxx@users.noreply.github.com>
2020-10-15 15:31:12 -0500

    Update PruebaGit.java

--- src/pruebaGit/PruebaGit.java
+++ src/pruebaGit/PruebaGit.java
@@ -2,14 +2,7 @@ package pruebaGit;
 
 public class PruebaGit {
 	public static void main( String[] args) {
-		System.out.println("asda");
-<<<<<<< HEAD
-		System.out.println("asd");
-		
-		System.out.println("");
-=======
-		System.out.println("asdasd");
-		System.out.println("asxxxxxxxxxxda");
->>>>>>> 48597c04d3b479d7d33e09811c00ddc26d48e422
+		System.out.println("Tienes que rogar a dios");
+		System.out.println("tú también");
 	}
 }
