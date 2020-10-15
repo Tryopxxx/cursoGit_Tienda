48597c04d3b479d7d33e09811c00ddc26d48e422
Tryopxxx <51171372+Tryopxxx@users.noreply.github.com>
2020-10-15 14:49:14 -0500

    Update PruebaGit.java

--- src/pruebaGit/PruebaGit.java
+++ src/pruebaGit/PruebaGit.java
@@ -3,5 +3,7 @@ package pruebaGit;
 public class PruebaGit {
 	public static void main( String[] args) {
 		System.out.println("asda");
+		System.out.println("asdasd");
+		System.out.println("asxxxxxxxxxxda");
 	}
 }
