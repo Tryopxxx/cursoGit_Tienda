4051dfb9d8c365445ab3d7de917884878f2f0ed9
Tryop-X <65966143+Tryop-X@users.noreply.github.com>
2020-10-18 17:15:50 -0500

    probando rama 01

--- src/pruebaGit/PruebaGit.java
+++ src/pruebaGit/PruebaGit.java
@@ -4,5 +4,6 @@ public class PruebaGit {
 	public static void main( String[] args) {
 		System.out.println("Tienes que rogar a dios");
 		System.out.println("tú también");
+		System.out.println("Estamos utilizando una rama-la primera");
 	}
 }
