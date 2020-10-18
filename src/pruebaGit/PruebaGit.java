1aa5ce1d0f17e032ac78d60c53203345b836dde7
Tryop-X <65966143+Tryop-X@users.noreply.github.com>
2020-10-18 17:25:37 -0500

    Agregamos actualización en la rama master

--- src/pruebaGit/PruebaGit.java
+++ src/pruebaGit/PruebaGit.java
@@ -4,5 +4,10 @@ public class PruebaGit {
 	public static void main( String[] args) {
 		System.out.println("Tienes que rogar a dios");
 		System.out.println("tú también");
+		
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("asdas");
+		}
 	}
 }
