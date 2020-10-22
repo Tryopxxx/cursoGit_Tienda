56d3063055de15130a0f8643ff98c54f13059a47
Tryop-X <65966143+Tryop-X@users.noreply.github.com>
2020-10-22 12:22:30 -0500

    modificaciones del jueves

--- src/pruebaGit/PruebaGit.java
+++ src/pruebaGit/PruebaGit.java
@@ -4,14 +4,12 @@ public class PruebaGit {
 	public static void main( String[] args) {
 		System.out.println("Tienes que rogar a dios");
 		System.out.println("tú también");
-<<<<<<< HEAD
 		
 		
 		for (int i = 0; i < 10; i++) {
 			System.out.println("asdas");
 		}
-=======
 		System.out.println("Estamos utilizando una rama-la primera");
->>>>>>> rama01
+
 	}
 }
