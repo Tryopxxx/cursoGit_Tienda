12388e036334a619d0e519fe1a10a902a757e733
Tryop-X <65966143+Tryop-X@users.noreply.github.com>
2020-10-22 15:46:21 -0500

    asdasdasd

--- src/pruebaGit/PruebaGit.java
+++ src/pruebaGit/PruebaGit.java
@@ -4,6 +4,10 @@ public class PruebaGit {
 	public static void main( String[] args) {
 		System.out.println("Tienes que rogar a dios");
 		System.out.println("tú también");
+		System.out.println("Tienes que rogar a dios");
+		System.out.println("tú también");
+		System.out.println("Tienes que rogar a dios");
+		System.out.println("tú también");
 		System.out.println("Estamos utilizando una rama-la primera");
 		for (int  x = 0; x < args.length; x++) {
 			System.out.println("awdasd");
